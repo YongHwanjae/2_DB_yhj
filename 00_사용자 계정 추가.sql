dfc525447bf5a2e783762a4b613727ca1e0c77dd
Yong Hwan Jae <ist635@naver.com>
2022-08-31 17:47:56 +0900

    Initial commit

---
+++ 00_사용자 계정 추가.sql
@@ -0,0 +1,19 @@
+-- 한줄 주석
+/*
+ * 범위주석
+ */
+
+
+-- SQL 하나 수행 : CTRL + ENTER
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 실습용 사용자 계정 생성
+CREATE  USER kh_yhj IDENTIFIED BY kh1234;
+
+-- 사용자 계정 권한 부여
+GRANT RESOURCE, CONNECT TO kh_yhj;
+
+-- 객체 생성(테이블 등) 공간 할당량 지정
+ALTER USER kh_yhj DEFAULT TABLESPACE SYSTEM
+QUOTA UNLIMITED ON SYSTEM;
+
